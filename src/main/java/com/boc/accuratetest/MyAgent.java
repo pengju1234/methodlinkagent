104d427e4486974fbb41c0737bd1d9b2e9f7e1b1
tom <tom@Lenovo-PC>
2020-08-25 16:22:07 +0800

    修改

--- src/main/java/com/boc/accuratetest/MyAgent.java
+++ src/main/java/com/boc/accuratetest/MyAgent.java
@@ -11,8 +11,12 @@ import java.io.PrintWriter;
 import java.lang.instrument.Instrumentation;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 public class MyAgent {
 	public static void premain(String args, Instrumentation inst){
@@ -69,12 +73,14 @@ public class MyAgent {
 		        	        StringBuilder sbWrite = new StringBuilder();
 		        	        boolean start = false;
 		        	        /*
-		        	         * 如果点击开始、结束直接，服务器没有被访问过，那么点击开始时，只会发送lastLine这最后一行
-		        	         * 无论什么情况，点击结束时，只会发送lastLine、和测试用例调用到的方法
+		        	         * 	1、第一次访问，即第一次点击开始时，只会发送lastLine这最后一行
+		        	         * 	2、第二次点击开始时，发送两次用例执行之间的方法，和lastLine最后一行
+		        	         * 	3、正常点击结束时，只会发送lastLine、和测试用例调用到的方法
 		        	         */
+		        	        String lastLineTemp = null;
 		        	        while ((tempStr = reader.readLine()) != null) { // 读整个文件，从最开始读
-		        	        	if(NumberOfVisits == 1) { // 首次访问
-		        	        		sbWrite.append(tempStr+"\n");
+		        	        	if(NumberOfVisits == 1) { // 首次访问，首次获取数据（只能是首次点击开始）
+		        	        		//
 		        	        	}else {
 		        	        		if(lastLine.equals(tempStr)) {
 		        	        			// 开始收集
@@ -85,13 +91,46 @@ public class MyAgent {
 		        	        			sbWrite.append(tempStr+"\n");
 		        	        		}
 		        	        	}
-		        	            lastLine = tempStr;// 记录最后一行
+		        	        	lastLineTemp = tempStr;// 临时记录最后一行
 		        	        }
+		        	        if(NumberOfVisits == 1) { // 首次访问，首次获取数据（只能是首次点击开始）
+	        	        		sbWrite.append(lastLine+"\n");
+	        	        	}
 		        	        pw.write(sbWrite.toString());
         	        		pw.flush();
 		        	        reader.close();
+		        	        lastLine = lastLineTemp;// 记录最后一行
 		        	        NumberOfVisits++;
 		        		}
+		        		/** 从测试服务器获取已测试方法	*/
+		        		if("chazhuang.txt_testedmethod".equals(sb.toString())) {
+		        			// 获取插桩文件
+		        			String gen = System.getProperty("user.dir");
+		        			BufferedReader reader = new BufferedReader(new FileReader(new File(gen+"/chazhuang.txt")));
+		        	        String tempStr = null;
+		        	        StringBuilder sbWrite = new StringBuilder();
+		        	        while ((tempStr = reader.readLine()) != null) { // 读整个文件，从最开始读
+		        	        	sbWrite.append(tempStr+"\r\n");
+		        	        }
+		        	        String contents = appendParams(sbWrite.toString()); // 补参数类型
+		        	        String[] lines = contents.split("\r\n"); // 原始数据的每一行（包前面的时间等等已经去掉了）
+		                    Set<String> linesSet = new HashSet<>(); // 去重
+		                    for (String line : lines) {
+		                    	if(lines.equals("java.lang.Thread.getStackTrace()")) {
+		                    		continue;
+		                    	}
+		                    	linesSet.add(line);
+		        			}
+		                    StringBuilder sb2 = new StringBuilder();
+		        	        for (String line : linesSet) {
+		        	        	sb2.append(line+"\r\n");
+							}
+		                    
+		        	        pw.write(sb2.toString());
+        	        		pw.flush();
+		        	        reader.close();
+		        		}
+		        		
 		        		/*else if("allMethodInfo.txt".equals(sb.toString())) {
 		        			// 获取目标项目所有的方法信息
 		        			String gen = System.getProperty("user.dir");
@@ -118,5 +157,34 @@ public class MyAgent {
 		        }
 			}
 		}).start();
+	}
+	/**
+	 * 	收集的原始数据，补上参数类型。
+	 * @param contents
+	 * @param testExampleId
+	 * @return
+	 */
+	public static String appendParams(String contents) {
+		StringBuilder sb = new StringBuilder();
+		String[] lines = contents.split("\r\n");
+		List<String> linesForFindParams = new ArrayList<>();
+		for (int i=0;i<lines.length;i++) { // 每一个方法的堆栈链 中 的每一行
+			// 截取（不要随机数、时间）
+			String line = lines[i].substring(lines[i].indexOf(".")+1, lines[i].length());
+			linesForFindParams.add(line);
+			if(!line.contains("(") && !line.contains("java.lang.Thread.getStackTrace")) {
+				// 补齐参数类型：后头找，使用第二个遇到的同包、同类、同方法名，并且有参数类型的（重载方法嵌套调用的、异步方法调用的，不能这样找，以后再解决）
+				for(int j=linesForFindParams.size()-1;j>=0;j--) {
+					if(linesForFindParams.get(j).contains(line+"(")) {
+						line = linesForFindParams.get(j);
+					}
+				}
+			}
+			if(!line.contains("(")) { // 没找到，暂时默认没有
+				line = line+"()";
+			}
+			sb.append(line+"\r\n");
+		}
+		return sb.toString();
 	}
 }
